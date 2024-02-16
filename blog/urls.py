b1c850fc38a877eb3b0af6602e08bd38c4083d96
tousif izaz <tousif_izaz@live.com>
2024-02-16 01:41:50 -0800

    added initial blog structure

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView
+urlpatterns = [
+ path("post/<int:pk>/", BlogDetailView.as_view(), name = "post_detail"),
+ path("post/new/", BlogCreateView.as_view(), name = "post_new"),
+ path("", BlogListView.as_view(), name="home"),
+ path("post/<int:pk>/edit", BlogUpdateView.as_view(), name="post_edit"),
+]
---
+++ blog/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from .models import Post
+
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+class BlogCreateView(CreateView): # new
+ model = Post
+ template_name = "post_new.html"
+ fields = ["title", "author", "body"]
+class BlogUpdateView(UpdateView):
+   model = Post
+   template_name = "post_edit.html"
+   fields = ["title", "body"]
